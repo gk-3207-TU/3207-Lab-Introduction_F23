9f77861408ba51b0610c88c5a75ab6119aa04820
Jericho R Orienza <93217801+Jericho-R-Orienza@users.noreply.github.com>
2023-08-31 23:59:50 -0400

    inserted #include "header file"

--- random.c
+++ random.c
@@ -1,18 +1,17 @@
-
+#include "random.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-
-//create the alphabet for function to use
-char alphabet[] = {
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-};
-
 char randchar(){
 
     srand(time(NULL));
+
+    //create the alphabet for function to use
+    char alphabet[] = {
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
 
     //get a random number from 0-25 to sesrve as the random index in our list
     int random_index = rand() % 26;
