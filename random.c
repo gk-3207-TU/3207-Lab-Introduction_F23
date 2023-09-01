d534f8392af512b5935fa205f9d02c30a11933bd
Jericho R Orienza <93217801+Jericho-R-Orienza@users.noreply.github.com>
2023-08-31 22:33:50 -0400

    create new file called "random.c" with function "randchar" that returns a random char from the alphabet

---
+++ random.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//create the alphabet for function to use
+char alphabet[] = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+};
+
+char randchar(){
+
+    srand(time(NULL));
+
+    //get a random number from 0-25 to sesrve as the random index in our list
+    int random_index = rand() % 26;
+
+    //create and define var char "letter" to hold our new randomized letter
+    char letter = alphabet[random_index];
+
+    return letter;
+}
+
